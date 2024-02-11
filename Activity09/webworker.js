32b0b070e471abea694f3a2ad0ee9d991043749a
mewpk <milzaza123@gmail.com>
2024-02-11 21:24:56 +0700

    Add functionality to generate and display random colors

--- Activity09/webworker.js
+++ Activity09/webworker.js
@@ -1,18 +1,10 @@
-self.addEventListener('message', function (e) {
-  if (e.data.action === 'start') {
-    clearInterval(self.interval);
-    const currentTime = new Date().toString();
-    const color = getRandomColor();
-    self.postMessage({ currentTime, color });
-    self.interval = setInterval(() => {
-      const currentTime = new Date().toString();
-      const color = getRandomColor();
-      self.postMessage({ currentTime, color });
-    }, 1000);
-  } else if (e.data.action === 'stop') {
-    clearInterval(self.interval);
-  }
-});
+const listColors = [];
+
+for (let i = 0; i < 6; i++) {
+  listColors.push(getRandomColor());
+}
+console.log(listColors);
+// 6 random colors are generated
 
 function getRandomColor() {
   const letters = '0123456789ABCDEF';
@@ -22,3 +14,24 @@ function getRandomColor() {
   }
   return color;
 }
+self.addEventListener('message', function (e) {
+  if (e.data.action === 'start') {
+    let round = 0;
+    clearInterval(self.interval);
+    const currentTime = new Date().toString();
+    const color = listColors[round];
+
+    self.postMessage({ currentTime, color });
+    self.interval = setInterval(() => {
+      round += 1;
+      if (round === listColors.length) {
+        round = 0;
+      }
+      const currentTime = new Date().toString();
+      const color = listColors[round];
+      self.postMessage({ currentTime, color });
+    }, 1000);
+  } else if (e.data.action === 'stop') {
+    clearInterval(self.interval);
+  }
+});
